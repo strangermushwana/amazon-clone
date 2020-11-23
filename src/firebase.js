fc4e86f416d2de1e912e658dc9256b9d6d4ab9e6
Stranger Mushwana <strangermushwana@gmail.com>
2020-11-23 15:01:12 +0200

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAP1yGynbhsUj-qDxIFksbHqu2HhYQ3hKw",
+    authDomain: "clone-2cd8e.firebaseapp.com",
+    databaseURL: "https://clone-2cd8e.firebaseio.com",
+    projectId: "clone-2cd8e",
+    storageBucket: "clone-2cd8e.appspot.com",
+    messagingSenderId: "109467136944",
+    appId: "1:109467136944:web:1be1cfff3745b1c499dc75",
+    measurementId: "G-24XDKK8K5M"
+  };
